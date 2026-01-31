216becdf77390e29cd920205b4476af33150f0d7
Madhumitha01-V <madhu61104@gmail.com>
2026-01-31 12:16:15 +0530

    Modified

--- route/auth_route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/authentication');
-
-router.post('/login', authController.login);    
-module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(express.json());
 const studentRoutes = require('./route/route');
 app.use('/student', studentRoutes);
 
-const authRoutes = require('./route/auth_route');
-app.use('/auth', authRoutes);
 
 app.listen(5000, () => {
     console.log('Server is running on port http://localhost:5000');
