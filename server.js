c5c1ec79de0712547c5e267420b5a57a03e4a0fa
Madhumitha01-V <madhu61104@gmail.com>
2026-01-31 09:29:53 +0530

    Modified and added data

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 const studentRoutes = require('./route/route');
 app.use('/student', studentRoutes);
+app.use('/auth', require('./route/auth_route'));
 
 app.listen(5000, () => {
     console.log('Server is running on port http://localhost:5000');
