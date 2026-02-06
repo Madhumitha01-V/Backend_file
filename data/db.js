bf96d202a05d37a1326a396e230224e57c0c138d
Madhumitha01-V <madhu61104@gmail.com>
2026-02-06 09:52:02 +0530

    Files Added

--- data/db.js
+++ data/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const createConnection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'M@dhu_01',
-    database:'demo_kec'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 createConnection.connect((err) => {
     if(err){
-        console.error('Error connecting to database:',err);
+        console.log('Error connecting to Database:', err);
     }else{
         console.log('Connected to Database');
     }
